54610884f5365ae9500f7534b554c29b8a16ac8f
shinyaee <ppeter1015@gmail.com>
2024-11-10 21:05:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -483,5 +483,5 @@ const songs = [
 function selectRandomSong() {
     const randomIndex = Math.floor(Math.random() * songs.length);
     const selectedSong = songs[randomIndex];
-    document.getElementById("song-result").textContent = `選ばれた曲は: ${selectedSong}`;
+    document.getElementById("song-result").textContent = `${selectedSong}`;
 }
